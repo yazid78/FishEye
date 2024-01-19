ba5d97ef949a8bcf7e06b8b7d222df930da5d91a
Yazid <yazid780@outlook.fr>
2024-01-19 18:43:09 +0100

    likes

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,6 +1,9 @@
 //Mettre le code JavaScript lié à la page photographer.html
 async function getPhotographerData(Idphotographer) {
-  let reponse = await fetch("../../data/photographers.json");
+  let reponse = await fetch("../../data/photographers.json", {
+    method: "GET",
+    mode: "cors",
+  });
   let data = await reponse.json();
 
   const photographer = data.photographers.find((p) => p.id === Idphotographer);
@@ -31,41 +34,71 @@ async function displayPhotographerDetails(photographer) {
   const photosContainer = document.createElement("div");
   photosContainer.classList.add("photographer_photos");
 
-//Images des photographes
-let reponse = await fetch("../../data/photographers.json");
-let data = await reponse.json();
-const medias = data.media.filter(
-  (media) => media.photographerId === photographer.id
-);
+  //Images des photographes
+  let reponse = await fetch("../../data/photographers.json");
+  let data = await reponse.json();
+  const medias = data.media.filter(
+    (media) => media.photographerId === photographer.id
+  );
 
-medias.forEach((media, index) => {
-  const mediaSource =
-    "assets/" + photographer.name + "/" + (media.image || media.video);
+  medias.forEach((media, index) => {
+    const mediaSource =
+      "assets/" + photographer.name + "/" + (media.image || media.video);
 
-  if (media.image) {
-    const photo = document.createElement("img");
-    photo.src = mediaSource;
-    photosContainer.appendChild(photo);
+    if (media.image) {
+      const mediaContainer = document.createElement("div");
+      const InformationsContainer = document.createElement("div");
+      InformationsContainer.classList.add("Informations");
 
-    // Ajoute un écouteur d'événements "click" à chaque image
-    photo.addEventListener("click", function () {
-      openModal(index); // Appelle la fonction openModal avec l'index de l'image
-    });
+      const photo = document.createElement("img");
+      photo.src = mediaSource;
 
-  } else if (media.video) {
-    const video = document.createElement("video");
-    video.src = mediaSource;
-    video.controls = true;
-    photosContainer.appendChild(video);
-  }
-});
+      const titleElement = document.createElement("h2");
+      titleElement.textContent = media.title;
+      titleElement.id = "title";
+      const priceElement = document.createElement("h2");
+      priceElement.textContent = media.price + "€";
+      priceElement.id = "price";
 
+      const likesElement = document.createElement("h2");
+      likesElement.innerHTML = media.likes + '<i class="fas fa-heart"></i>';
+      let a = media.likes;
+      let rouge = likesElement.querySelector(".fas.fa-heart")
+      let b = rouge.style.color ='red'
+      console.log("ok ok");
+      const heartIcon = likesElement.querySelector(".fas.fa-heart");
+      console.log(heartIcon);
+      heartIcon.addEventListener("click", function () {
+       a++;
+      console.log(a);
+      likesElement.innerHTML = media.likes + b;
+      });
+      console.log(media.image);
+
+      mediaContainer.appendChild(photo);
+      InformationsContainer.appendChild(titleElement);
+      InformationsContainer.appendChild(priceElement);
+      InformationsContainer.appendChild(likesElement);
+
+      photosContainer.appendChild(mediaContainer);
+      mediaContainer.appendChild(InformationsContainer);
+
+      // Ajoute un écouteur d'événements "click" à chaque image
+      photo.addEventListener("click", function () {
+        openModal(index); // Appelle la fonction openModal avec l'index de l'image
+      });
+    } else if (media.video) {
+      const video = document.createElement("video");
+      video.src = mediaSource;
+      video.controls = true;
+      photosContainer.appendChild(video);
+    }
+  });
 
   const mainSection = document.getElementById("main");
   mainSection.appendChild(photosContainer);
-
-
 }
+//CAROUSSEL//
 let currentPhotoIndex = 0;
 let modalImages = [];
 
@@ -94,8 +127,7 @@ function changePhoto(offset) {
   modalImage.src = modalImages[currentPhotoIndex].src;
 }
 
-// Ajoutez cet écouteur d'événements dans la fonction init
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
   if (event.key === "Escape") {
     closeCarroussel();
   } else if (event.key === "ArrowLeft") {
@@ -105,7 +137,22 @@ document.addEventListener("keydown", function(event) {
   }
 });
 
+//filtre//
 
+const sortSelect = document.getElementById("sortSelect");
+let images = document.querySelectorAll(".photographer_photos img");
+
+sortSelect.addEventListener("change", () => {
+  const selectedValue = sortSelect.value;
+
+  if (selectedValue === "Popularité") {
+    console.log("Trier par popularité ici");
+  } else if (selectedValue === "date") {
+    console.log("Trier par date ici");
+  } else if (selectedValue === "title") {
+    console.log("Trier par titre ici");
+  }
+});
 
 function init() {
   const urlParams = new URLSearchParams(window.location.search);
